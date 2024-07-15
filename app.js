9b11489dc8ff891e11e669161a4c1e12350b7c20
Makarand Vijaykumar More <E12007@tbspl.local>
2024-07-15 14:40:32 +0530

    chapter 3

--- app.js
+++ app.js
@@ -1,24 +1,42 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+//React.createElement => Object => HTML Element - Renders
 const heading = React.createElement(
   "h4",
   { id: "title" },
   "Hello Makarand from React!"
 );
 
-const parent = React.createElement(
-  "div",
-  { id: "parent" },[
-    React.createElement("div", { id:"child" },[
-    React.createElement("h4", { id:"subTitle" }, "I am a h4 tag"),
-    React.createElement("h5", { id:"subTitle" }, "I am a h5 tag")
-    ])
-  ]
+//JSX => Babel transpiles it to React.createElement => React Element - JS Object => HTML Element - Renders
+const jsxHeading = ( //React Element
+  <h4 className="title">Persons:</h4>
+);
+
+const javaScript = (1000); //javascript
+
+//Two types of React Component:
+//Class based Component - OLD
+//Functional Compnent - NEW
+const FunctComponent1 = () => (
+  <h2 className="heading">Hi Mak</h2>
+);
+
+const FunctComponent2 = () => (
+  <div className="container">
+    <FunctComponent1 /> {/* Component into Component is called as 'Component Composition' */}
+
+    <h3>Total: {javaScript}</h3> {/* Insert javaScript into the JSX Functional Component */}
+
+    {jsxHeading} {/* Insert ReactElement into the JSX Functional Component */}
+
+    <h5 className="title">01. Jack</h5>
+  </div> 
 );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
-// root.render([heading,parent]);
 
-console.log([heading,parent]);
+root.render(jsxHeading); //Render React Element
+root.render(<FunctComponent2 />) //Render React Functional Component
+
+console.log([heading,jsxHeading]);
