1b17d3449fa4106f0bc6c97c1ae98b0860792eb1
Makarand Vijaykumar More <E12007@tbspl.local>
2024-07-19 18:21:59 +0530

    chapter 4 - wip

--- app.js
+++ app.js
@@ -1,42 +1,109 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import Styles from "./style.css";
 
-//React.createElement => Object => HTML Element - Renders
-const heading = React.createElement(
-  "h4",
-  { id: "title" },
-  "Hello Makarand from React!"
-);
+/* Components:
+Header
+  - Logo, Nav
+Body
+  - Search
+  - Blog Container
+    - Blog Card
+      - Img, Blog Name, Ratings, time, etc.
+Footer
+  - Copyright, Links, Address, Contact
+*/
 
-//JSX => Babel transpiles it to React.createElement => React Element - JS Object => HTML Element - Renders
-const jsxHeading = ( //React Element
-  <h4 className="title">Persons:</h4>
-);
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo"><img src="https://www.tradebulls.in/images/tradebulls-logo.svg" alt="Logo" className="" width="340" height="70" /></div>
+      <div className="nav-items">
+        <ul>
+          <li><a href="" title="">Home</a></li>
+          <li><a href="" title="">About Us</a></li>
+          <li><a href="" title="">Contact Us</a></li>
+          <li><a href="" title="">Cart</a></li>
+        </ul>
+      </div>
+    </div>
+  )
+}
 
-const javaScript = (1000); //javascript
+const styleBlogCard = { //javascript use for styling 
+  backgroundColor: '#eee',
+  border: '1px solid #666',
+}
 
-//Two types of React Component:
-//Class based Component - OLD
-//Functional Compnent - NEW
-const FunctComponent1 = () => (
-  <h2 className="heading">Hi Mak</h2>
-);
+const BlogCard = (props) => {
+  const {blogData} = props;
+  return (
+    <div className="blog-card" style={styleBlogCard}>
+      <div className="blog-img">
+        <img src={blogData.imgUrl} alt="Blog Img" width="184" height="150" />
+      </div>    
+      <h4>{blogData.title}</h4>
+      <h5>{blogData.blogShortDesc}</h5>
+      <div className="d-flex blog-ratings">
+        <div className="col-2">{blogData.blogRatings} ratings</div>
+        <div className="col-2">{blogData.timeToRead} min Read</div>
+      </div>
+    </div>
+  )
+}
 
-const FunctComponent2 = () => (
-  <div className="container">
-    <FunctComponent1 /> {/* Component into Component is called as 'Component Composition' */}
+const blogObj = [
+  {
+    "title": "Everything You Need to Know About Sovereign Gold Bond Scheme",
+    "timeToRead": "3",
+    "blogShortDesc": "In the dynamic world of investments, gold remains a timeless treasure, valued for its enduring appeal and intrinsic worth.",
+    "imgUrl": "https://uat-cmsapi.tradebulls.in/uploads/Sovereign_Gold_Bonds_Your_Ultimate_Guide_c6eb10424f.svg",
+    "imgAlt": "",
+    "blogRatings": "4.5"
+  },
+  {
+    "title": "Sovereign Gold Bond vs. Gold Mutual Funds vs Gold ETF : A Comparison",
+    "timeToRead": "5",
+    "blogShortDesc": "India, a nation renowned for its deep-rooted fascination with gold, is witnessing a significant shift in the way its...",
+    "imgUrl": "https://uat-cmsapi.tradebulls.in/uploads/SGB_vs_Gold_ETF_vs_Gold_MF_A_Comparison_fc0f4eb89d.svg",
+    "imgAlt": "",
+    "blogRatings": "4.2"
+  },
+];
 
-    <h3>Total: {javaScript}</h3> {/* Insert javaScript into the JSX Functional Component */}
+const Body = () => {
+  return (
+    <div className="container">
+      <div className="search-bar">Search</div>
+      <div className="blog-container">
+      {
+        blogObj?.map((item , index)=>(
+          <BlogCard blogData={item} />
+        ))
+      }
+      </div>
+    </div>    
+  )
+}
 
-    {jsxHeading} {/* Insert ReactElement into the JSX Functional Component */}
 
-    <h5 className="title">01. Jack</h5>
-  </div> 
-);
+const Footer = () => {
+  return (
+    <div className="footer"></div>
+  )
+}
+
+const AppLayout = () => {
+  return  (
+    <div className="app-layout">
+      <Header />
+      <Body />
+      <Footer />
+    </div>
+  )
+}
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(jsxHeading); //Render React Element
-root.render(<FunctComponent2 />) //Render React Functional Component
-
-console.log([heading,jsxHeading]);
+root.render(<AppLayout />) //Render React Functional Component
